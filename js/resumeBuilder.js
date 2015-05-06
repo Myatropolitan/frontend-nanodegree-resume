96087290a45369e2e0c2cbfafed8b87f58e4b8fc
Myatropolitan <miaghori@gmail.com>
2015-05-06 09:02:47 +0100

    Fixed based on first submission review. Quotations & encapsulations

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,212 +1,206 @@
 //********* BIO OBJECT *********
 var bio = {
-	'name' : 'Mia Okabe',
-	'role' : 'Web Developer',
-	'contacts' : {
-		'mobile' : '+44-0000-00-0000',
-		'email' : 'miaghori@gmail.com',
-		'github' : 'myatropolitan',
-		'twitter' : 'myaa24',
-		'location' : 'London, UK'
+	"name": "Mia Okabe",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "+44-0000-00-0000",
+		"email": "miaghori@gmail.com",
+		"github": "myatropolitan",
+		"twitter": "myaa24",
+		"location": "London, UK"
 	},
-	'welcomeMessage' : 'Welcome to my resume site',
-	'skills' : [ 'HTML5', 'CSS3', 'JavaScript', 'C++' ],
-	'biopic' : './images/profile.jpg'
-}
+	"welcomeMessage": "Welcome to my resume site",
+	"skills": [ "HTML5", "CSS3", "JavaScript", "C++" ],
+	"biopic": "./images/profile.jpg",
+	"display": function(){
+		//Format Bio
+		var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+		$('#header').prepend(formattedRole);
 
-bio.display = function() {
-	//Format Bio
-	var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-	$('#header').prepend(formattedRole);
+		var formattedName = HTMLheaderName.replace('%data%', bio.name);
+		$('#header').prepend(formattedName);
 
-	var formattedName = HTMLheaderName.replace('%data%', bio.name);
-	$('#header').prepend(formattedName);
+		var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+		$('#topContacts').append(formattedMobile);
+		$('#footerContacts').append(formattedMobile);
 
-	var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-	$('#topContacts').append(formattedMobile);
-	$('#footerContacts').append(formattedMobile);
+		var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+		$('#topContacts').append(formattedEmail);
+		$('#footerContacts').append(formattedEmail);
 
-	var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
-	$('#topContacts').append(formattedEmail);
-	$('#footerContacts').append(formattedEmail);
+		var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+		$('#topContacts').append(formattedGithub);
+		$('#footerContacts').append(formattedGithub);
 
-	var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
-	$('#topContacts').append(formattedGithub);
-	$('#footerContacts').append(formattedGithub);
+		var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
+		$('#topContacts').append(formattedTwitter);
+		$('#footerContacts').append(formattedTwitter);
 
-	var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
-	$('#topContacts').append(formattedTwitter);
-	$('#footerContacts').append(formattedTwitter);
+		var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+		$('#topContacts').append(formattedLocation);
+		$('#footerContacts').append(formattedLocation);
 
-	var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
-	$('#topContacts').append(formattedLocation);
-	$('#footerContacts').append(formattedLocation);
+		var formattedBioPic = HTMLbioPic.replace('%data%', bio.biopic);
+		$('#header').append(formattedBioPic);
 
-	var formattedBioPic = HTMLbioPic.replace('%data%', bio.biopic);
-	$('#header').append(formattedBioPic);
-
-	var formattedWelcomMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
-	$('#header').append(formattedWelcomMsg);
+		var formattedWelcomMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+		$('#header').append(formattedWelcomMsg);
 
 
-	if(bio.skills.length > 0){
-		$('#header').append(HTMLskillsStart);
+		if(bio.skills.length > 0){
+			$('#header').append(HTMLskillsStart);
 
-		for (skill in bio.skills)
-		{
-			var formattedSkills = HTMLskills.replace('%data%', bio.skills[skill]);
-			$('#header').append(formattedSkills);
+			for (skill in bio.skills)
+			{
+				var formattedSkills = HTMLskills.replace('%data%', bio.skills[skill]);
+				$('#header').append(formattedSkills);
+			}
 		}
 	}
-
 }
 
 //********* EDUCATION OBJECT *********
 var education = {
-	'schools' : [
+	"schools": [
 		{
-			'name' : 'Keio University',
-			'location' : 'Kanagwa, Japan',
-			'degree' : 'BA',
-			'majors' : [ 'Media Design' ],
-			'dates' : '2005',
-			'url' : 'http://www.keio.ac.jp/index-en.html'
+			"name": "Keio University",
+			"location": "Kanagwa, Japan",
+			"degree": "BA",
+			"majors": [ "Media Design" ],
+			"dates": "2005",
+			"url": "http://www.keio.ac.jp/index-en.html"
 		}
 	],
-	'onlineCourses' : [
+	"onlineCourses": [
 		{
-			'title' : 'Intro to HTML and CSS',
-			'school' : 'Udacity',
-			'date' : '2015',
-			'url' : 'https://www.udacity.com/course/viewer#!/c-ud304-nd'
+			"title": "Intro to HTML and CSS",
+			"school": "Udacity",
+			"date": "2015",
+			"url": "https://www.udacity.com/course/viewer#!/c-ud304-nd"
 		},
 		{
-			'title' : 'Responsive Web Design Fundamentals',
-			'school' : 'Udacity',
-			'date' : '2015',
-			'url' : 'https://www.udacity.com/course/viewer#!/c-ud893-nd'
+			"title": "Responsive Web Design Fundamentals",
+			"school": "Udacity",
+			"date": "2015",
+			"url": "https://www.udacity.com/course/viewer#!/c-ud893-nd"
 		},
 		{
-			'title' : 'Responsive Images',
-			'school' : 'Udacity',
-			'date' : '2015',
-			'url' : 'https://www.udacity.com/course/viewer#!/c-ud882-nd'
+			"title": "Responsive Images",
+			"school": "Udacity",
+			"date": "2015",
+			"url": "https://www.udacity.com/course/viewer#!/c-ud882-nd"
 		}
-	]
-}
+	],
+	"display": function(){
+		for(school in education.schools){
+			$('#education').append(HTMLschoolStart);
 
-education.display = function() {
-	for(school in education.schools){
-		$('#education').append(HTMLschoolStart);
+			var formattedName = HTMLschoolName.replace('%data%', education.schools[school].name);
+			$('.education-entry:last').append(formattedName);
 
-		var formattedName = HTMLschoolName.replace('%data%', education.schools[school].name);
-		$('.education-entry:last').append(formattedName);
+			var formattedDegree = HTMLschoolDegree.replace('%data%', education.schools[school].degree);
+			$('.education-entry:last').append(formattedDegree);
 
-		var formattedDegree = HTMLschoolDegree.replace('%data%', education.schools[school].degree);
-		$('.education-entry:last').append(formattedDegree);
+			var formattedDate = HTMLschoolDates.replace('%data%', education.schools[school].dates);
+			$('.education-entry:last').append(formattedDate);
 
-		var formattedDate = HTMLschoolDates.replace('%data%', education.schools[school].dates);
-		$('.education-entry:last').append(formattedDate);
+			var formattedLocation = HTMLschoolLocation.replace('%data%', education.schools[school].location);
+			$('.education-entry:last').append(formattedLocation);
 
-		var formattedLocation = HTMLschoolLocation.replace('%data%', education.schools[school].location);
-		$('.education-entry:last').append(formattedLocation);
-
-		for(major in education.schools[school].majors){
-			var formattedMajor = HTMLschoolMajor.replace('%data%', education.schools[school].majors[major]);
-			$('.education-entry:last').append(formattedMajor);
+			for(major in education.schools[school].majors){
+				var formattedMajor = HTMLschoolMajor.replace('%data%', education.schools[school].majors[major]);
+				$('.education-entry:last').append(formattedMajor);
+			}
 		}
-	}
-	if(education.onlineCourses.length > 0){
-		$('#education').append(HTMLonlineClasses);
-	}
-	for(course in education.onlineCourses){
-		$('#education').append(HTMLschoolStart);
+		if(education.onlineCourses.length > 0){
+			$('#education').append(HTMLonlineClasses);
+		}
+		for(course in education.onlineCourses){
+			$('#education').append(HTMLschoolStart);
 
-		var formattedTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[course].title);
-		$('.education-entry:last').append(formattedTitle);
+			var formattedTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[course].title);
+			$('.education-entry:last').append(formattedTitle);
 
-		var formattedSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[course].school);
-		$('.education-entry:last').append(formattedSchool);
+			var formattedSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[course].school);
+			$('.education-entry:last').append(formattedSchool);
 
-		var formattedDate = HTMLonlineDates.replace('%data%', education.onlineCourses[course].date);
-		$('.education-entry:last').append(formattedDate);
+			var formattedDate = HTMLonlineDates.replace('%data%', education.onlineCourses[course].date);
+			$('.education-entry:last').append(formattedDate);
 
-		var formattedURL = HTMLonlineURL.replace('%data%', education.onlineCourses[course].url);
-		$('.education-entry:last').append(formattedURL);
+			var formattedURL = HTMLonlineURL.replace('%data%', education.onlineCourses[course].url);
+			$('.education-entry:last').append(formattedURL);
+		}
 	}
 }
 
 //********* WORK OBJECT *********
 var work = {
-	'jobs' : [
+	"jobs": [
 		{
-			'employer' : 'Amazon Japan K.K.',
-			'title' : 'Logisitics IT project manager',
-			'location' : 'Tokyo, Japan',
-			'dates' : 'April 2011 - April 2013',
-			'description' : 'Responsible for the technical implementation of courier projects and new services, architecting scalable configurations for the company’s complex transportation network, and serving as the technical front line support for the transportation team.'
+			"employer": "Amazon Japan K.K.",
+			"title": "Logisitics IT project manager",
+			"location": "Tokyo, Japan",
+			"dates": "April 2011 - April 2013",
+			"description": "Responsible for the technical implementation of courier projects and new services, architecting scalable configurations for the company’s complex transportation network, and serving as the technical front line support for the transportation team."
 		},
 		{
-			'employer' : 'Hewlett-Packard Japan, Ltd',
-			'title' : 'Software Engineer',
-			'location' : 'Tokyo, Japan',
-			'dates' : 'April 2006 - March 2011',
-			'description' : 'Accountable for managing multiple projects, with duties ranging from defining requirements, resourcing, scheduling, coding, testing, to providing technical support. Experience in agile and waterfall development.'
+			"employer": "Hewlett-Packard Japan, Ltd",
+			"title": "Software Engineer",
+			"location": "Tokyo, Japan",
+			"dates": "April 2006 - March 2011",
+			"description": "Accountable for managing multiple projects, with duties ranging from defining requirements, resourcing, scheduling, coding, testing, to providing technical support. Experience in agile and waterfall development."
 		}
-	]
-}
+	],
+	"display": function(){
+		for(job in work.jobs){
+			$('#workExperience').append(HTMLworkStart);
 
-work.display = function() {
-	for(job in work.jobs){
-		$('#workExperience').append(HTMLworkStart);
+			var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+			var formatteWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+			var formattedEmployerTitle = formattedEmployer + formatteWorkTitle;
+			$('.work-entry:last').append(formattedEmployerTitle);
 
-		var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
-		var formatteWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
-		var formattedEmployerTitle = formattedEmployer + formatteWorkTitle;
-		$('.work-entry:last').append(formattedEmployerTitle);
+			var formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
+			$('.work-entry:last').append(formattedWorkLocation);
 
-		var formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
-		$('.work-entry:last').append(formattedWorkLocation);
+			var formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
+			$('.work-entry:last').append(formattedWorkDates);
 
-		var formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
-		$('.work-entry:last').append(formattedWorkDates);
-
-		var formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
-		$('.work-entry:last').append(formattedWorkDescription);
+			var formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
+			$('.work-entry:last').append(formattedWorkDescription);
+		}
 	}
 }
 
 //********* PROJECTS OBJECT *********
 var projects = {
-	'projects' : [
+	"projects": [
 		{
-			'title' : 'Nanodegree Project 1: Portfolio',
-			'dates' : 'March 2015 - April 2015',
-			'description' : 'Portfolio site project for Udacity Front End Developer Nanodegree',
-			'images' : [ './images/project1.png', './images/project1_2.png']
+			"title": "Nanodegree Project 1: Portfolio",
+			"dates": "March 2015 - April 2015",
+			"description": "Portfolio site project for Udacity Front End Developer Nanodegree",
+			"images": [ "./images/project1.png", "./images/project1_2.png"]
 		}
-	]
-}
+	],
+	"display": function(){
+		for(project in projects.projects){
+			$('#projects').append(HTMLprojectStart);
 
-projects.display = function(){
-	for(project in projects.projects){
-		$('#projects').append(HTMLprojectStart);
+			var formattedProjectTitle = HTMLprojectTitle.replace('%data%', projects.projects[project].title);
+			$('.project-entry:last').append(formattedProjectTitle);
 
-		var formattedProjectTitle = HTMLprojectTitle.replace('%data%', projects.projects[project].title);
-		$('.project-entry:last').append(formattedProjectTitle);
+			var formattedProjectDates = HTMLprojectDates.replace('%data%', projects.projects[project].dates);
+			$('.project-entry:last').append(formattedProjectDates);
 
-		var formattedProjectDates = HTMLprojectDates.replace('%data%', projects.projects[project].dates);
-		$('.project-entry:last').append(formattedProjectDates);
+			var formattedDescription = HTMLprojectDescription.replace('%data%', projects.projects[project].description);
+			$('.project-entry:last').append(formattedDescription);
 
-		var formattedDescription = HTMLprojectDescription.replace('%data%', projects.projects[project].description);
-		$('.project-entry:last').append(formattedDescription);
-
-		for(image in projects.projects[project].images){
-			var formattedProjectImage = HTMLprojectImage.replace('%data%', projects.projects[project].images[image]);
-			$('.project-entry:last').append(formattedProjectImage);
+			for(image in projects.projects[project].images){
+				var formattedProjectImage = HTMLprojectImage.replace('%data%', projects.projects[project].images[image]);
+				$('.project-entry:last').append(formattedProjectImage);
+			}
 		}
 	}
-
 }
 
 //Display all items
